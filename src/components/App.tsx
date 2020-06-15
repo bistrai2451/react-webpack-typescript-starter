d9e0bdf4110a8b829efa5425b24df91f418fbaa4
St0Sy5 <drumm3r09@gmail.com>
2020-06-15 17:27:20 -0500

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface AppProps {
+    userName: string;
+    lang: string;
+}
+
+export const App: React.FC<AppProps> = ({ userName, lang }) => (
+    <h1>
+        Hi {userName}! Welcome to the {lang} Webpack starter project with React.
+    </h1>
+);
