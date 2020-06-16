fe6577c71f9a731372146e35f1353f92efd9ad8a
Andre Bistrain <bistrai2451@gmail.com>
2020-06-15 20:49:27 -0500

    generalizing text

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
 ReactDOM.render(
-    <App userName={'Andre'} lang={'TypeScript'} />,
+    <App userName={'Person'} lang={'TypeScript'} />,
     document.getElementById('app')
 );
